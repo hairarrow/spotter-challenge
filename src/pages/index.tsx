6eed5378f59bed157c68990c8a702f5fbfc6cc1f
Emmanuel Herrero <emmanuelherrero@gmail.com>
2023-08-14 13:30:40 -0700

    spotter favicon <3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,10 @@ export default function Home() {
       <Head>
         <title>Hello Spotter!</title>
         <meta name="description" content="Made with <3 for Spotter" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="https://spotter.la/favicon-32x32.png?v=4a4629bbd528fe9bd72f2efdc6d8442e"
+        />
       </Head>
       <main className="min-h-screen bg-black">
         <BeatSheet />
